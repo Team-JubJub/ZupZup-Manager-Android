4d7a06c3a73bd5a908ad759b1d1972fb8f8b82c7
Jiwoo <a980917a@naver.com>
2023-03-18 18:32:07 +0900

    [Feat] #26 - 예약상세 화면 수량 +,- 기능 구현

--- app/src/main/java/com/example/zupzup_manager/ui/reservationdetail/ReservationDetailBindingAdapter.kt
+++ app/src/main/java/com/example/zupzup_manager/ui/reservationdetail/ReservationDetailBindingAdapter.kt
@@ -1,24 +1,26 @@
 package com.example.zupzup_manager.ui.reservationdetail
 
 import android.graphics.drawable.GradientDrawable
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.zupzup_manager.R
 import com.example.zupzup_manager.domain.models.CartModel
 import com.example.zupzup_manager.ui.common.ReservationStateMapper
 import com.example.zupzup_manager.ui.common.toDecimalFormat
 import com.example.zupzup_manager.ui.common.toDetailDateFormat
 import com.example.zupzup_manager.ui.common.toTimeFormat
-import com.example.zupzup_manager.ui.reservationdetail.models.ReservationDetail
+import com.example.zupzup_manager.ui.reservationdetail.models.ReservationDetailViewType
 
 
 @BindingAdapter("reservationDetailBody")
 fun bindReservationDetailListToRecyclerView(
     recyclerView: RecyclerView,
-    dataList: List<ReservationDetail>
+    dataList: List<ReservationDetailViewType>
 ) {
-    (recyclerView.adapter as ReservationDetailRcvAdapter).submitList(dataList)
+    (recyclerView.adapter as ReservationDetailRcvAdapter).submitList(dataList.toList())
 }
 
 @BindingAdapter("state")
@@ -59,5 +61,34 @@ fun bindReservationTotalPriceToTextView(
 ) {
     if (cartList != null) {
         textView.text = cartList.sumOf { it.salesPrice * it.amount }.toDecimalFormat()
+    }
+}
+
+@BindingAdapter("bindingHelper", "itemId", "tvConfirmedAmount", "orderAmount")
+fun bindBindingHelperToAmountButton(
+    ivBtnModifyAmount: ImageView,
+    bindingHelper: ReservationDetailBindingHelper,
+    itemId: Long,
+    tvConfirmedAmount: TextView,
+    orderAmount: Int
+) {
+    ivBtnModifyAmount.setOnClickListener {
+        when (ivBtnModifyAmount.id) {
+            R.id.btn_plus -> {
+                if (tvConfirmedAmount.text.toString().toInt() < orderAmount) {
+                    bindingHelper.onPlusCartItemConfirmedAmountBtnClick(itemId)
+                    tvConfirmedAmount.text =
+                        tvConfirmedAmount.text.toString().toInt().plus(1).toString()
+                }
+            }
+            R.id.btn_minus -> {
+                if (tvConfirmedAmount.text.toString().toInt() > 0) {
+                    bindingHelper.onMinusCartItemConfirmedAmountBtnClick(itemId)
+                    tvConfirmedAmount.text =
+                        tvConfirmedAmount.text.toString().toInt().minus(1).toString()
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/zupzup_manager/ui/reservationdetail/models/ReservationDetailHeaderModel.kt
+++ app/src/main/java/com/example/zupzup_manager/ui/reservationdetail/models/ReservationDetailHeaderModel.kt
@@ -3,5 +3,6 @@ package com.example.zupzup_manager.ui.reservationdetail.models
 data class ReservationDetailHeaderModel(
     val title : String,
     val state : String,
+    val storeId : Long,
     val reserveId : Long
 )
