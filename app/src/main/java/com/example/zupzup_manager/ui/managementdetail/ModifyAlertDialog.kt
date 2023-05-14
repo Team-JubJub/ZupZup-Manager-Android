9376c521d9ccbdae7a0aaad55c82bf25be97d20c
XETA <csh4519@naver.com>
2023-05-15 02:01:14 +0900

    [Feat] alert dialog 구현

---
+++ app/src/main/java/com/example/zupzup_manager/ui/managementdetail/ModifyAlertDialog.kt
@@ -0,0 +1,41 @@
+package com.example.zupzup_manager.ui.managementdetail
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
+import android.widget.Button
+import com.example.zupzup_manager.R
+import com.example.zupzup_manager.domain.models.StoreModel
+
+class ModifyAlertDialog(context: Context) {
+
+    lateinit var listener: MerchandiseDialogClickedListener
+    lateinit var btnModify: Button
+    lateinit var btnCancel: Button
+
+    interface MerchandiseDialogClickedListener {
+        fun onClicked()
+    }
+
+    private val dlg = Dialog(context)
+
+    fun modify() {
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dlg.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dlg.setContentView(R.layout.dialog_alert)
+
+        btnModify = dlg.findViewById(R.id.btn_modify)
+        btnModify.setOnClickListener {
+            listener.onClicked()
+            dlg.dismiss()
+        }
+
+        btnCancel = dlg.findViewById(R.id.btn_cancel_modify)
+        btnCancel.setOnClickListener {
+            dlg.dismiss()
+        }
+        dlg.show()
+    }
+}
