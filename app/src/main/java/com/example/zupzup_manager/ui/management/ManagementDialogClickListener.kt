897066e8978d4529ebd7bd19d91a910e454edaba
xet-a <csh4519@naver.com>
2023-07-13 20:37:42 +0900

    [Fix] 중복 파일 삭제

--- app/src/main/java/com/example/zupzup_manager/ui/management/ManagementDialogClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.zupzup_manager.ui.management
-
-import com.example.zupzup_manager.domain.models.MerchandiseModel
-
-interface ManagementDialogClickListener {
-
-    fun changeState(state: String)
-
-}
