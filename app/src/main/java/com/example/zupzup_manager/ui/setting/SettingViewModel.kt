7d22312453c8250b2ffd599b88c1cc27ab0458a9
xet-a <csh4519@naver.com>
2023-07-17 23:32:18 +0900

    [Fix] Setting Fragment 정비 및 코드 정리

--- app/src/main/java/com/example/zupzup_manager/ui/setting/SettingViewModel.kt
+++ app/src/main/java/com/example/zupzup_manager/ui/setting/SettingViewModel.kt
@@ -1,20 +1,44 @@
 package com.example.zupzup_manager.ui.setting
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.zupzup_manager.domain.DataResult
+import com.example.zupzup_manager.domain.models.StoreModel
+import com.example.zupzup_manager.domain.usecase.GetStoreDetailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingViewModel @Inject constructor(
+    private val getStoreDetailUseCase: GetStoreDetailUseCase,
 ) : ViewModel() {
 
+    init {
+        getStoreInfo(2)
+    }
+
+    private val initStoreModel = StoreModel(0,"","","",
+        Pair(0.0, 0.0),"",listOf(),listOf(), Pair(0, 0))
     // 가게 상태 DB에서 읽어서 상태 초기화 필요, 임시로 기본값 false 지정
-    private var _storeStatus = MutableLiveData(false)
-    val storeStatus = _storeStatus
+    private var _storeInfo = MutableStateFlow<StoreModel>(initStoreModel)
+    val storeInfo = _storeInfo
+
 
     fun changeStoreStatus() {
-        _storeStatus.value = !_storeStatus.value!!
+//        _storeStatus.value = !_storeStatus.value!!
+    }
+
+    private fun getStoreInfo(storeId: Long) {
+        viewModelScope.launch {
+            getStoreDetailUseCase(storeId).collect {
+                if (it is DataResult.Success) {
+                    _storeInfo.emit(it.data)
+                } else {
+                    _storeInfo.emit(initStoreModel)
+                }
+            }
+        }
     }
 }
