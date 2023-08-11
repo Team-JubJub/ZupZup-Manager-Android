9f750d723619e4c3ad9d50142ec606d39a7ac979
xet-a <csh4519@naver.com>
2023-08-12 02:27:58 +0900

    [Refactor] 이전 파일 삭제

--- app/src/main/java/com/example/zupzup_manager/ui/management/clicklistener/ManagementDialogClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.zupzup_manager.ui.management.clicklistener
-
-interface ManagementDialogClickListener {
-    // Dialog 내부에서 클릭할 때 모드 전환하는 클릭 리스너
-    fun changeState(state: String)
-    fun navigateToMerchandiseAdd()
-
-}
